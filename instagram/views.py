eff8cb0f28cbc6739db3851c7c54302a2d5f5597
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-12-14 11:33:33 +0300

    implementing the authentication forms

--- instagram/views.py
+++ instagram/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/accounts/login/')
 def welcome(request):
-  return render(request, 'welcome.html')
+  return render(request, 'authentication/authentication.html')
