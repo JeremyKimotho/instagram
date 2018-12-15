e43de87f0859016cfc3c9d78600b8c7b520db5f9
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-12-15 11:07:45 +0300

    experimenting with authentication

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-  url('^$', views.welcome)
+  url('^$', views.default)
 ]
 
 if settings.DEBUG:
--- instagram/views.py
+++ instagram/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-def welcome(request):
+@login_required(login_url='/accounts/register')
+def default(request):
   return render(request, 'actual/home.html')
