abb31ae88e5afa464414c3b2a2e85998e92a34bf
Jeremy Kimotho <jeremykimotho@yahoo.com>
2018-12-17 08:57:34 +0300

    have added the methods inside of the class of save, delete, update

--- instagram/models.py
+++ instagram/models.py
@@ -7,6 +7,20 @@ class Profile(models.Model):
   bio = models.TextField(default='Single and ready to mingle', max_length=500, blank=True)
   photo = models.ImageField(blank=True, upload_to='photos/')
 
+  def save_profile(self):
+    self.save()
+
+  def delete_profile(self):
+    self.delete()
+
+  def update_profile(self, id, updated_bio):
+    profile = Profile.objects.filter(pk=id).update(bio = updated_bio)
+    return profile.save_profile()
+
+  @classmethod
+  def search_profile(cls, search):
+    profile = cls.objects.filter(user__user__icontains=search)
+
   def __str__(self):
     return self.user
 
@@ -16,4 +30,20 @@ class Image(models.Model):
   image_caption = models.CharField(max_length = 60)
   posted_at = models.DateTimeField(auto_now_add=True)
   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+  def save_image(self):
+    self.save()
+
+  def delete_image(self):
+    self.delete()
+
+  def update_profile(self, id, updated_caption):
+    image = Image.objects.filter(pk=id).update(image_caption = updated_caption)
+    return image.save_image()
+
+  @classmethod
+  def get_images(cls):
+    images = cls.objects.all()
+    return images
+
 
